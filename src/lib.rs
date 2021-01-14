b5eddb3779be756602dcbdd6413ea8cc0d8e0428
rami3l <rami3l@outlook.com>
2021-01-14 11:43:54 +0100

    test: move all tests to doctest

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,57 @@
+//! A simple library that allows you to write multiline string in a more elegant way.
+
 // https://github.com/scala/scala/blob/39148e4ec34a5c53443dd1b25ceec2308cd097fe/src/library/scala/collection/StringOps.scala#L739-L763
 pub trait StripMargin {
     /// For every line in this string, strip a leading prefix consisting of blanks or control characters,
     /// followed by `margin_char` from the line.
+    ///
+    /// # Examples
+    /// ```rust
+    /// use stripmargin::StripMargin;
+    /// assert_eq!(
+    ///     r#"
+    ///     *Hello,
+    ///     *  world!
+    ///     *"#
+    ///     .strip_margin_with('*'),
+    ///     "\nHello,\n  world!\n",
+    /// );
+    /// ```
+    ///
+    /// Please note that a non-whitespace margin won't get stripped:
+    /// ```rust
+    /// use stripmargin::StripMargin;
+    /// assert_eq!(
+    ///     "Hello * world!".strip_margin_with('*'),
+    ///     "Hello * world!",
+    /// );
+    /// ```
     fn strip_margin_with(&self, margin_char: char) -> String;
 
     /// For every line in this string, strip a leading prefix consisting of blanks or control characters,
     /// followed by `'|'` from the line.
+    ///
+    /// # Examples
+    /// ```rust
+    /// use stripmargin::StripMargin;
+    /// assert_eq!(
+    ///     r#"
+    ///     |Hello,
+    ///     |  world!
+    ///     |"#
+    ///     .strip_margin(),
+    ///     "\nHello,\n  world!\n",
+    /// );
+    /// ```
+    ///
+    /// Please note that a non-whitespace margin won't get stripped:
+    /// ```rust
+    /// use stripmargin::StripMargin;
+    /// assert_eq!(
+    ///     "Hello | world!".strip_margin(),
+    ///     "Hello | world!",
+    /// );
+    /// ```
     fn strip_margin(&self) -> String {
         self.strip_margin_with('|')
     }
@@ -23,54 +69,5 @@ impl<S: AsRef<str>> StripMargin for S {
                 }
             })
             .collect()
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn empty_string() {
-        assert_eq!(
-            "".strip_margin(),
-            "",
-            "strip_margin should not modify an empty string"
-        );
-    }
-
-    #[test]
-    fn non_blank_prefix() {
-        assert_eq!(
-            "Hello | world!".strip_margin(),
-            "Hello | world!",
-            "strip_margin should not modify a non-blank prefix"
-        );
-    }
-
-    #[test]
-    fn blank_prefix() {
-        assert_eq!(
-            r#"
-            |Hello,
-            |  world!
-            |"#
-            .strip_margin(),
-            "\nHello,\n  world!\n",
-            "strip_margin should eliminate blank prefixes"
-        );
-    }
-
-    #[test]
-    fn custom_margin_char() {
-        assert_eq!(
-            r#"
-            *Hello,
-            *  world!
-            *"#
-            .strip_margin_with('*'),
-            "\nHello,\n  world!\n",
-            "strip_margin_with should handle custom margin_char"
-        );
     }
 }
